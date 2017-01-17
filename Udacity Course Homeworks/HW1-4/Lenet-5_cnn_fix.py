d35f3b9c80725e8d7e680c6c1de2e0d8121897ec
张麟 <zlake1992@gmail.com>
2017-01-17 17:11:26 +0800

    lenet-5

---
+++ Udacity Course Homeworks/HW1-4/Lenet-5_cnn_fix.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  8 22:09:19 2017
+
+@author: LinZhang
+"""
+
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+import numpy as np
+import tensorflow as tf
+from six.moves import cPickle as pickle
+from six.moves import range
+import numpy as np
+
+
+def reformat(dataset, labels):
+    dataset = dataset.reshape(
+        (-1, image_size, image_size, num_channels)).astype(np.float32)
+    labels = (np.arange(num_labels) == labels[:, None]).astype(np.float32)
+    return dataset, labels
+
+
+def accuracy(predictions, labels):
+    return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+            / predictions.shape[0])
+
+# def initial variables:
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+# def basic operation in cnn:
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+image_size = 28
+num_labels = 10
+num_channels = 1  # grayscale
+
+batch_size = 16
+patch_size = 5
+depth = 16
+num_hidden = 1024
+
+pickle_file = 'notMNIST.pickle'
+
+with open(pickle_file, 'rb') as f:
+    save = pickle.load(f)
+    train_dataset = save['train_dataset']
+    train_labels = save['train_labels']
+    valid_dataset = save['valid_dataset']
+    valid_labels = save['valid_labels']
+    test_dataset = save['test_dataset']
+    test_labels = save['test_labels']
+    del save  # hint to help gc free up memory
+    print('Training set', train_dataset.shape, train_labels.shape)
+    print('Validation set', valid_dataset.shape, valid_labels.shape)
+    print('Test set', test_dataset.shape, test_labels.shape)
+
+train_dataset, train_labels = reformat(train_dataset, train_labels)
+valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+test_dataset, test_labels = reformat(test_dataset, test_labels)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
+
+# a small network with two convolutional layers, followed by one fully connected layer
+graph = tf.Graph()
+
+with graph.as_default():
+    # Input data.
+    tf_train_dataset = tf.placeholder(
+        tf.float32, shape=(batch_size, image_size, image_size, num_channels))
+    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+    tf_valid_dataset = tf.constant(valid_dataset)
+    tf_test_dataset = tf.constant(test_dataset)
+    keep_prob = tf.placeholder(tf.float32)
+
+    # Variables.
+    layer1_weights = weight_variable([patch_size, patch_size, num_channels, 32])
+    layer1_biases = bias_variable([32])
+    
+    layer2_weights = weight_variable([patch_size, patch_size, 32, 64])
+    layer2_biases = bias_variable([64])
+    
+    layer3_weights = weight_variable([image_size // 4 * image_size // 4 * 64, num_hidden])
+    layer3_biases = bias_variable([num_hidden])
+    
+    layer4_weights = weight_variable([num_hidden, num_labels])
+    layer4_biases = bias_variable([num_labels])
+
+
+    # Model.
+    def model(data,use_dropout = False):
+        conv1 = tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='SAME')
+        hidden1 = tf.nn.relu(conv1 + layer1_biases)
+        hidden1_pool = max_pool_2x2(hidden1)
+        conv2 = tf.nn.conv2d(hidden1_pool, layer2_weights, [1, 1, 1, 1], padding='SAME')
+        hidden2 = tf.nn.relu(conv2 + layer2_biases)
+        hidden2_pool = max_pool_2x2(hidden2)
+        shape = hidden2_pool.get_shape().as_list()
+        reshape = tf.reshape(hidden2_pool, [shape[0], shape[1] * shape[2] * shape[3]])
+        hidden3 = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
+        if (use_dropout):
+            return tf.matmul(tf.nn.dropout(hidden3,keep_prob), layer4_weights) + layer4_biases
+        else:
+            return tf.matmul(hidden3, layer4_weights) + layer4_biases
+
+
+    # Training computation.
+    logits = model(tf_train_dataset,use_dropout = True)
+    loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+    # learning rate decay
+    global_step = tf.Variable(0, trainable=False)
+    starter_learning_rate = 0.1
+    learning_rate = tf.train.exponential_decay(0.05,
+                                               global_step, 100, 0.95, staircase=True)
+
+    # Optimizer.
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+    # Predictions for the training, validation, and test data.
+    train_prediction = tf.nn.softmax(logits)
+    valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+    test_prediction = tf.nn.softmax(model(tf_test_dataset))
+
+    num_steps = 1001
+
+with tf.Session(graph=graph) as session:
+    tf.global_variables_initializer().run()
+    print('Initialized')
+    for step in range(num_steps):
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        feed_dict = {tf_train_dataset: batch_data, tf_train_labels: batch_labels, keep_prob:0.5}
+        _, l, predictions = session.run(
+            [optimizer, loss, train_prediction], feed_dict=feed_dict)
+        if (step % 50 == 0):
+            print('Minibatch loss at step %d: %f' % (step, l))
+            print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
+            print('Validation accuracy: %.1f%%' % accuracy(
+                valid_prediction.eval(), valid_labels))
+    print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
--- Udacity Course Homeworks/HW1-4/lenet-5_cnn.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 17 11:37:00 2017
-
-@author: lakezhang
-"""
-
